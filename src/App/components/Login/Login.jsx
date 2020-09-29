f475efbab82ba3e7b0cfd7ae741547a448e52228
x320608 <xian.oliveirataboada@plexus.es>
2020-09-29 08:13:58 +0200

    login navigation function fixed

--- src/App/components/Login/Login.jsx
+++ src/App/components/Login/Login.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Switch, Route, Link,
-} from 'react-router-dom';
-import Register from '../Register';
+import { Link } from 'react-router-dom';
 
 let mensajeLogin = '';
 
@@ -11,45 +8,38 @@ const Login = () => {
     mensajeLogin = 'patata';
   }
   return (
-    <Router>
-      <div className="gridRegister">
-        <label htmlFor="user" className="nombre">
-          <span>Usuario</span>
-          <input className="box" id="user" />
-        </label>
+    <div className="gridRegister">
+      <label htmlFor="user" className="nombre">
+        <span>Usuario</span>
+        <input className="box" id="user" />
+      </label>
 
-        <br />
-        <br />
+      <br />
+      <br />
 
-        <label htmlFor="password">
-          <span>Contraseña</span>
-          <input type="password" className="box" id="password" />
-        </label>
+      <label htmlFor="password">
+        <span>Contraseña</span>
+        <input type="password" className="box" id="password" />
+      </label>
 
-        <br />
-        <br />
-        <span>
-          <a href="register">
-            <Link to="/register">
-              Haz click aquí para crear un nuevo usuario
-            </Link>
-          </a>
-        </span>
-        <br />
-        <br />
-        {/* <p>{{mensajeLogin}}</p> */}
-        <div className="botones">
-          <button type="button" className="button">
-            Entrar
-          </button>
-        </div>
-        <Switch>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
+      <br />
+      <br />
+      <span>
+        <a href="register">
+          <Link to="/register">
+            Haz click aquí para crear un nuevo usuario
+          </Link>
+        </a>
+      </span>
+      <br />
+      <br />
+      {/* <p>{{mensajeLogin}}</p> */}
+      <div className="botones">
+        <button type="button" className="button">
+          <Link to="/productList">Entrar</Link>
+        </button>
       </div>
-    </Router>
+    </div>
   );
 };
 
