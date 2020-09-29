6c75159e533bfcf1fe7e6bbdaa09aca66ea6c29a
x320608 <xian.oliveirataboada@plexus.es>
2020-09-29 19:04:13 +0200

    top bar link to product list

--- src/App/components/TopBar/TopBar.jsx
+++ src/App/components/TopBar/TopBar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopBar = () => (
-  <div className="app-top-bar">
-    <a href="productList">
-      <h1>Tienda de teléfonos</h1>
-    </a>
-    <a className="button fancy-button" href="cart">
-      <i className="material-icons">shopping_cart</i>
-      Checkout
-    </a>
+  <div>
+    <div className="app-top-bar">
+      <Link to="/productList">
+        <h1>Tienda de teléfonos</h1>
+      </Link>
+      <a className="button fancy-button" href="cart">
+        <i className="material-icons">shopping_cart</i>
+        Checkout
+      </a>
+    </div>
   </div>
 );
 
